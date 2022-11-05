2751883d348d7cfaa5ac713023e281a91b4ffcce
nchabli <nchabli@student.42lyon.fr>
2022-11-05 19:44:54 +0100

    commitage

--- ex01/includes/PhoneBook.hpp
+++ ex01/includes/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: nchabli <nchabli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 14:56:36 by nchabli           #+#    #+#             */
-/*   Updated: 2022/11/03 14:54:48 by nchabli          ###   ########.fr       */
+/*   Updated: 2022/11/04 15:28:14 by nchabli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,9 @@ class PhoneBook
 		~PhoneBook(void);
 		
 	private :
-		void	_add_contact();
+		void		_add_contact();
+		void		_search_contact();
+		std::string	_take_contact_word(std::string word);
 	
 	public :
 		// void	print_contact_infos(int pos);
--- ex01/srcs/class/PhoneBook.cpp
+++ ex01/srcs/class/PhoneBook.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nchabli <nchabli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 14:54:03 by nchabli           #+#    #+#             */
-/*   Updated: 2022/11/03 22:38:58 by nchabli          ###   ########.fr       */
+/*   Updated: 2022/11/04 15:47:04 by nchabli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,7 @@ std::string	PhoneBook::get_entry(void)
 	if (_buffer == "ADD")
 		_add_contact();
 	if (_buffer == "SEARCH")
-		std::cout<<"searching contact"<<std::endl;
-		// _search_contact();
+		_search_contact();
 	return (_buffer);
 }
 
@@ -42,27 +41,72 @@ void	PhoneBook::_add_contact()
 {
 	
 	std::cout<<"First name :"<<std::endl;
-	getline(std::cin, _buffer);
+	_buffer = "";
+	while (_buffer.empty())
+		getline(std::cin, _buffer);
 	_contact[_write_on].set_first_name(_buffer);
+	_buffer = "";
 	std::cout<<"Last name :"<<std::endl;
-	getline(std::cin, _buffer);
+	while (_buffer.empty())
+		getline(std::cin, _buffer);
 	_contact[_write_on].set_last_name(_buffer);
+	_buffer = "";
 	std::cout<<"Nickname :"<<std::endl;
-	getline(std::cin, _buffer);
+	while (_buffer.empty())
+		getline(std::cin, _buffer);
 	_contact[_write_on].set_nickname(_buffer);
+	_buffer = "";
 	std::cout<<"Phone number :"<<std::endl;
-	getline(std::cin, _buffer);
+	while (_buffer.empty())
+		getline(std::cin, _buffer);
 	_contact[_write_on].set_phone_number(_buffer);
+	_buffer = "";
 	std::cout<<"Darkest secret :"<<std::endl;
-	getline(std::cin, _buffer);
+	while (_buffer.empty())
+		getline(std::cin, _buffer);
 	_contact[_write_on].set_darkest_secret(_buffer);
+	_buffer = "";
+	_contact[_write_on].set_index(_write_on + 1);
+	_buffer = "";
 	std::cout<<"nb contact : " << _nb_contact << " / 1st contact :" << _contact[0].get_first_name()<< "written on : " <<_write_on<<std::endl;
-	if (_nb_contact <= 8)
+	if (_nb_contact < 8)
 		_nb_contact++;
-	if (_write_on == 8)
+	if (_write_on == 7)
 		_write_on = 0;
 	else
 		_write_on++;
+}
+
+std::string	PhoneBook::_take_contact_word(std::string word)
+{
+	if (word.size() > 10)
+	{
+		word.replace(9, word.size(), ".");
+	}
+	else
+	{
+		int i = 0;
+		std::string	spaces;
+		while(i < (10 - (int)word.size()))
+		{
+			spaces = spaces.append(" ");
+			i++;
+		}
+		word = spaces.append(word);
+	}
+	return (word);
+}
+
+void	PhoneBook::_search_contact()
+{
+	int	i = 0;
+	std::cout << "     INDEX|FIRST NAME| LAST NAME|  NICKNAME" << std::endl;
+	while (i < _nb_contact)
+	{
+		std::cout << "         "<< _contact[i].get_index() << "|"<< _take_contact_word(_contact[i].get_first_name())
+			<< "|" << _take_contact_word(_contact[i].get_last_name()) << "|" << _take_contact_word(_contact[i].get_nickname()) <<std::endl;
+		i++;
+	}
 }
 
 // void	PhoneBook::_add_contact(std::string contact_name, int contact_pos)
